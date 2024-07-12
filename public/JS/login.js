925c1603a67e83be7ceae9272529c1fb19f18baa
happy2023year <ttap3@happy2023year.com>
2024-07-12 15:39:48 +0530

    cancelled redirect

--- public/JS/login.js
+++ public/JS/login.js
@@ -20,7 +20,7 @@ loginForm.addEventListener('submit', async e => {
                 const data = await response.json();
                 localStorage.setItem('token', data.token);
                 alert('Login successful!');
-                window.location.href = '/dashboard'; // I should create a page for landing
+                // window.location.href = '/dashboard'; // I should create a page for landing
             } else {
                 const errorMessage = await response.text();
                 throw new Error(errorMessage);
